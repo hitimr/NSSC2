919dede5df24c3603f2880548603048ab82bee74
hitimr <hitimr@gmail.com>
2021-03-14 19:29:34 +0100

    test

--- Ex1/Hiti/testfile.py
+++ Ex1/Hiti/testfile.py
@@ -1,17 +1,6 @@
 # TODO
 
 
-<<<<<<< HEAD
 Ändeurng 1
 
-TestÄnderung
-=======
-testestestasdftest
-=======
-
-
-
-asdf
-Ändeurng 1
-asdfasdfasdfa
->>>>>>> ce3dbf916cf31281e2cac6d35e5285d8785ac88e
+TestÄnderungsgfef
