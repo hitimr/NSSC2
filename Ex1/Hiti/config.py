b592ca0c046f3e706816a68465112c9d55059403
hitimr <hitimr@gmail.com>
2021-03-17 07:41:10 +0100

    typo

--- Ex1/Hiti/config.py
+++ Ex1/Hiti/config.py
@@ -4,6 +4,6 @@ import os
 WORKSPACE_DIR = os.path.dirname(os.path.realpath(__file__)) + "/"
 
 TEST_DIR = WORKSPACE_DIR + "test/"
-TASK2_DIR = WORKSPACE_DIR + "Task2/"
+TASK2_DIR = WORKSPACE_DIR + "task2/"
 TASK2_BUILD_DIR = WORKSPACE_DIR + "out/"
 
