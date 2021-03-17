04dea6e0b5a8d0fb5699b07c7cca945cb28e67c4
hitimr <hitimr@gmail.com>
2021-03-17 07:00:49 +0100

    created unit test environment

---
+++ Ex1/Hiti/config.py
@@ -0,0 +1,8 @@
+import os 
+
+
+WORKSPACE_DIR = os.path.dirname(os.path.realpath(__file__)) + "/"
+TEST_DIR = WORKSPACE_DIR + "test"
+TASK2_DIR = WORKSPACE_DIR + "Task2/"
+TASK2_BUILD_DIR = WORKSPACE_DIR + "out/"
+
