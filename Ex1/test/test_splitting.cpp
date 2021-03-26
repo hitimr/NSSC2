247a5c1f9e96cec06e56f5a757bb23e6f4c9c7af
hitimr <hitimr@gmail.com>
2021-03-26 20:59:28 +0100

    created test environment

---
+++ Ex1/test/test_splitting.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "splitting.hpp"
+
+using namespace std;
+
+int g_n_processes;
+int g_dim;
+size_t g_resolution;
+
+
+int main()
+{
+
+    g_n_processes = 5;
+    g_dim = DIM1;
+    g_resolution = 17;
+    return 0;
+
+
+    cout << "All Tests passed" << endl;
+}
