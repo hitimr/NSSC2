583826007d8bc28606fd5b79991e0b7edc9b19f7
troublemaker-1 <m.renoldner@gmail.com>
2021-04-05 19:43:11 +0200

    plot files first try

--- Ex1/Reno/plots.py
+++ Ex1/Reno/plots.py
@@ -4,9 +4,14 @@ import pandas as pd
 import numpy
 
 
-dataframe=pd.read_csv('log_2021-04-03-10-23-03.csv')
+dataframe=pd.read_csv('testlog.csv')
 
-print(dataframe.head(0))
+print(dataframe.head((0)))
+
+
+for i in range(len(dataframe.head(0))):
+    print(dataframe.head(i))
+
 
 
 
