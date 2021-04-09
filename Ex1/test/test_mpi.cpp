f49dd70fe87467059f6a02ee27017336f3b5ad14
Markus Hickel <markus.hickel@gmx.at>
2021-04-09 19:42:47 +0200

    added get_neighbour

--- Ex1/test/test_mpi.cpp
+++ Ex1/test/test_mpi.cpp
@@ -24,10 +24,74 @@ MPI_Comm g_topo_com;
 
 int get_neighbours(vector<int> coords, int direction)
 {
-    // **** REMOVE ONCE THIS ONCE get_neighbours() IS IMPLEMENTED ****
-    // This function only exists so the compiler doesnt cry
-    return -1;
+    int topProc, botProc, leftProc, rightProc, returnProc;
+	MPI_Cart_shift(g_topo_com, 1, 1, &botProc, &topProc);
+	MPI_Cart_shift(g_topo_com, 0, 1, &leftProc, &rightProc);
+	if (direction == TOP)
+	{
+		returnProc=topProc;
+	}
+	else if (direction == BOTTOM)
+	{
+		returnProc=botProc;
+	}
+	else if (direction == LEFT)
+    {
+        returnProc=leftProc;
+    }
+	else if (direction == RIGHT)
+    {
+        returnProc=rightProc;
+    }
+	return returnProc;
 }
+
+int test_getNeighbours_2D()
+{
+    MPI_Barrier(MPI_COMM_WORLD); 
+    if(g_my_rank == MASTER) cout << "Testing getNeighbours for 1D" << endl;
+
+    if(g_my_rank == 5)
+    {
+    assert(get_neighbours({0,5}, TOP) == NO_NEIGHBOUR);
+    assert(get_neighbours({0,5}, BOTTOM) == 4);
+    }
+
+    if(g_my_rank == 4)
+    {   
+    assert(get_neighbours({0,4}, TOP) == 5);
+    assert(get_neighbours({0,4}, BOTTOM) == 3);
+    }
+
+    if(g_my_rank == 3)
+    {   
+    assert(get_neighbours({0,3}, TOP) == 4);
+    assert(get_neighbours({0,3}, BOTTOM) == 2);
+    }
+
+    if(g_my_rank == 2)
+    {
+    assert(get_neighbours({0,2}, TOP) == 3);
+    assert(get_neighbours({0,2}, BOTTOM) == 1);
+    }
+
+    if(g_my_rank == 1)
+    {
+    assert(get_neighbours({0,1}, TOP) == 2);
+    assert(get_neighbours({0,1}, BOTTOM) == 0);
+    }
+    if(g_my_rank == 0)
+    {
+    assert(get_neighbours({0,0}, TOP) == 1);
+    assert(get_neighbours({0,0}, BOTTOM) == NO_NEIGHBOUR);
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    if(g_my_rank == MASTER) cout << "OK" << endl;
+    return SUCCESS;
+}
+
+
 
 
 int test_getNeighbours_1D()
@@ -35,24 +99,41 @@ int test_getNeighbours_1D()
     MPI_Barrier(MPI_COMM_WORLD);
     if(g_my_rank == MASTER) cout << "Testing getNeighbours for 1D" << endl;
 
+	if(g_my_rank == 5)
+	{
     assert(get_neighbours({0,5}, TOP) == NO_NEIGHBOUR);
     assert(get_neighbours({0,5}, BOTTOM) == 4);
+	}
 
+	if(g_my_rank == 4)
+    {   
     assert(get_neighbours({0,4}, TOP) == 5);
     assert(get_neighbours({0,4}, BOTTOM) == 3);
+	}
 
+    if(g_my_rank == 3)
+    {   
     assert(get_neighbours({0,3}, TOP) == 4);
     assert(get_neighbours({0,3}, BOTTOM) == 2);
+	}
 
+	if(g_my_rank == 2)
+    {
     assert(get_neighbours({0,2}, TOP) == 3);
     assert(get_neighbours({0,2}, BOTTOM) == 1);
-
+	}
+	
+	if(g_my_rank == 1)
+    {
     assert(get_neighbours({0,1}, TOP) == 2);
     assert(get_neighbours({0,1}, BOTTOM) == 0);
+	}
 
+	if(g_my_rank == 0)
+    {
     assert(get_neighbours({0,0}, TOP) == 1);
     assert(get_neighbours({0,0}, BOTTOM) == NO_NEIGHBOUR);
-
+	}
 
     for(int i = 0; i < g_n_processes; i++)
     {
@@ -109,6 +190,40 @@ int test_1D()
 int test_2D()
 {
     // Tests with 2D Grid
+    // Tests with 1D Grid
+    // rearrange processes to vertically stacked topology
+    if(g_my_rank == MASTER) cout << "Rearanging to vertical topology" << endl;
+    MPI_Barrier(MPI_COMM_WORLD);
+    vector<int> dims(2);
+    vector<int> coords(2); // holds the rank's coordinates within the topology
+    dims[0] = 2;
+    dims[1] = g_n_processes/2;
+    vector<int> periods = {false, false};
+    MPI_Cart_create(MPI_COMM_WORLD, 2, dims.data(), periods.data(), true, &g_topo_com); // https://www.mpich.org/static/docs/v3.3/www3/MPI_Cart_create.html
+    MPI_Cart_coords(g_topo_com, g_my_rank, 2, coords.data());
+
+    cout << "Rank " << g_my_rank << " now has coords (" << coords[0] << "|" << coords[1] << ")" << endl;
+
+
+    /* Ranks should now be ordered:
+
+    Rank 5 -> Top
+    Rank 4
+    Rank 3
+    Rank 2
+    Rank 1
+    Rank 0 -> Bot
+
+    */
+
+
+    // Start tests
+    MPI_Barrier(g_topo_com);
+
+    assert(test_getNeighbours_1D() == SUCCESS);
+
+    // TODO:
+    //assert(test_bordertypes_1D() == SUCCESS);
 
     // TODO: 2D tests
     return SUCCESS;
@@ -125,7 +240,7 @@ int main(int argc, char * argv[])
     assert(g_n_processes == 6 && "Error: This test requires 6 processes!");
 
     assert(test_1D() == SUCCESS);
-    assert(test_2D() == SUCCESS);
+    //assert(test_2D() == SUCCESS);
 
     MPI_Finalize();
 
