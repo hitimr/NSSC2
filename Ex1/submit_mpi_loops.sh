be5a5d2828f55abeeec62fc4cf8e90bf5071daa4
hitimr <hitimr@gmail.com>
2021-04-10 17:43:45 +0200

    only even numbe rof procs

--- Ex1/submit_mpi_loops.sh
+++ Ex1/submit_mpi_loops.sh
@@ -9,7 +9,7 @@ if command -v sinfo  2>/dev/null # if on cluster
 then
     module load mpi/openmpi-x86_64
     module load pmi/pmix-x86_64
-    mpiprocs=( 1 2 3 4 5 6 8 10 12 14 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80 )
+    mpiprocs=( 1 2 4 6 8 10 12 14 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80 )
     resolutions=( 125 500 2000 4000 )
     folder="out/datacluster"
     mkdir -p $folder    
