8593612e8daf4cd7d5cb6b5e1649418ce713fe6f
hitimr <hitimr@gmail.com>
2021-04-23 20:25:25 +0200

    yapf formatting

--- Ex2/src/domain.py
+++ Ex2/src/domain.py
@@ -1,26 +1,26 @@
-#include parent folder 
-import os,sys,inspect
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))) 
-parentdir = os.path.dirname(currentdir) 
-sys.path.insert(0,parentdir) 
- 
+#include parent folder
+import os, sys, inspect
+
+currentdir = os.path.dirname(
+    os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
 
 import numpy as np
 from config import *
 
 
 class Domain:
-    particle_count = int    # Number of particles in the box
-    length = float          # length of the box
-    std_dev = float         # standard deviation of the velocities
-    pos = np.ndarray        # positional data of the particles
-    vel = np.ndarray        # velocity data of the particles
+    particle_count = int  # Number of particles in the box
+    length = float  # length of the box
+    std_dev = float  # standard deviation of the velocities
+    pos = np.ndarray  # positional data of the particles
+    vel = np.ndarray  # velocity data of the particles
 
     def __init__(self):
         self.particle_count = -1
         self.length = -1
         self.std_dev = -1
-        
 
     def fill(self, particle_count, length, std_dev):
         """Fill the domain with particles as specified in Task 2
@@ -29,21 +29,21 @@ class Domain:
             particle_count (int): number of particles
             length (float): length of the simulation box
             std_dev (float): standard deviation for the velocity distribution
-        """   
-        assert(isinstance(particle_count, int))
-        assert(particle_count > 0)
-        assert(length > 0)
-        assert(std_dev > 0)
+        """
+        assert (isinstance(particle_count, int))
+        assert (particle_count > 0)
+        assert (length > 0)
+        assert (std_dev > 0)
 
         self.particle_count = particle_count
         self.length = length
-        self.std_dev = std_dev  
+        self.std_dev = std_dev
 
-        self.pos = np.random.rand(self.particle_count, 3)   # TODO: replace with custom distribution. see description
-        self.vel = np.random.rand(self.particle_count, 3)   # TODO: replace gaussian distribution
+        # TODO: replace with custom distribution. see description
+        self.pos = np.random.rand(self.particle_count, 3)
+        self.vel = np.random.rand(self.particle_count, 3)
 
-        assert(self.pos.shape == self.vel.shape)
-
+        assert (self.pos.shape == self.vel.shape)
 
     def read_from_file(self, fileName):
         """Fill the domain with data from a file
@@ -52,12 +52,11 @@ class Domain:
             fileName (str): path to file
         """
 
-        assert(particle_count > 0)
-        assert(length > 0)
-        assert(self.pos.shape == self.vel.shape)
+        assert (particle_count > 0)
+        assert (length > 0)
+        assert (self.pos.shape == self.vel.shape)
 
         return
-
 
     def write_to_file(self, fileName, comment=""):
         """Write domain data to a file
@@ -65,12 +64,11 @@ class Domain:
         Args:
             fileName (str): path to file
             comment (str): arbitrary comment/description for Line 2
-        """       
+        """
 
         return
 
 
-        
 if __name__ == "__main__":
     domain = Domain()
     domain.fill(10, 1, 1)
