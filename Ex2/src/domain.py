26fe2d0abce300c533977ef642b42eeb77249795
hitimr <hitimr@gmail.com>
2021-04-26 21:33:59 +0200

    added init routines for pos and vel

--- Ex2/src/domain.py
+++ Ex2/src/domain.py
@@ -43,21 +43,30 @@ class Domain:
         self.length = length
         self.std_dev = std_dev
 
-        # TODO: replace with custom distribution. see description -> Reno
-        self.pos = numpy.random.rand(self.particle_count, 3)
-        self.vel = numpy.random.rand(self.particle_count, 3) * 2.0 - 1
+        self.initialize_pos()
+        self.initialize_vel()
 
         assert (self.pos.shape == self.vel.shape)
 
-    
+    def initialize_pos(self):
+        # TODO, Reno: replace with custom distribution from Task 2.1
+        self.pos = numpy.ndarray((self.particle_count, 3))
+
+        spacing = self.length / (self.particle_count)
+        for i in range(self.particle_count):
+            for j in range(3):
+                self.pos[i][j] = spacing / 2 + i*spacing # + 1/2 to avoid placing particles at (0|0)
+
+    def initialize_vel(self):
+        # TODO, Reno: replace with custom distriburtion from Task 2.3 and 2.4
+        self.vel = numpy.random.rand(self.particle_count, 3) * 2.0 - 1
 
     def minimizeEnergy(self):
         grad_Epot = jit(grad(self.E_pot), static_argnums=(1,1))
         result = minimize(
             self.E_pot, 
             self.pos,
-            method='CG',
-            jac=grad_Epot
+            method='CG'
             )
         new_pos = result.x
         new_pos.shape = (int(len(new_pos) / 3), 3)
@@ -68,8 +77,8 @@ class Domain:
     def E_pot(self, pos):
         # TODO: optimize this routine
         E = 0
-        pos=self.pos
-        # pos is has MxN dimensions
+
+        # pos has MxN dimensions
         if len(pos.shape) == 2:
             for i in range(len(pos)):
                 for j in range(i+1):
@@ -90,8 +99,6 @@ class Domain:
     def V_LJ(self, r):      
         # With a uniform distribution its possible that r is almost 0
         # So for now we just ignore that case
-        # TODO: remove once we have a better distribution -> Reno
-        if r < 0.01: r = 0.01
 
         return 4 * (pow(0.25 / r, 12) - pow(1 / r, 6))
 
@@ -99,6 +106,7 @@ class Domain:
 
     def read_from_file(self, fileName):
         """Fill the domain with data from a file
+        #TODO, Hickel: read n-th block 
 
         Args:
             fileName (str): path to file
@@ -122,6 +130,8 @@ class Domain:
     def write_to_file(self, fileName, comment=""):
         """Write domain data to a file
 
+        #TODO, Hickel: Append Blocks
+
         Args:
             fileName (str): path to file
             comment (str): arbitrary comment/description for Line 2
@@ -139,14 +149,12 @@ class Domain:
 
 if __name__ == "__main__":
     domain = Domain()
-    domain.fill(10, 1, 1)
+    domain.fill(5, 1, 1)
     domain.write_to_file("test","bla")
     domain.read_from_file("test")
+
+
     print("generated positions:")
     print(domain.pos)
     print(f"Position of first particle:")
     print(domain.pos[0])
-
-    print(domain.E_pot(domain.pos))
-    domain.minimizeEnergy()
-    print(domain.E_pot(domain.pos))
