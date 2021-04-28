e24cbb0ea0edf67e85b95ad783661ebfc9894559
hitimr <hitimr@gmail.com>
2021-04-29 01:26:29 +0200

    integrated new Epot algorithm

--- Ex2/src/domain.py
+++ Ex2/src/domain.py
@@ -18,26 +18,15 @@ from jax import grad, jit
 from config import *
 from src.misc import *
 
+def Epot(x):
+    x = to3D(x)
+    n = len(x)
+    broadcasted = x[:, jnp.newaxis, :] - x   # calculate the distance between all particles while utilizing jnp.broadcast
+    broadcasted = broadcasted.reshape(n*n,3)
+    r = jnp.linalg.norm(broadcasted, axis=1)  # calculate the euclidian norm of all distances
+    r6 =  jnp.nan_to_num(jnp.power(r, -6), posinf=0.0)
+    return 4*(2*jnp.dot(r6,r6) - r6.sum())
 
-def Epot(pos):    
-    # scipy optimize passes flattened array to E_pot. If pos is 1D transform
-    # it to 3D and back again after the calculation has finished
-    #if len(pos.shape) == 1:
-    v = to3D(pos)
-
-    #E = jnp.triu(scipy.spatial.distance.cdist(v, v, metric=VLJ)).sum() # LOL that actually works :D
-
-
-    
-    # Old way to calculate the potential. Still left in here to verify results
-    # Calculate the potential energy for all pairs of molecules. Skip cases
-    # where i=j and distances that have already been calculated
-    E = 0
-    for i in range(len(v)):
-        for j in range(i + 1, len(v)):
-            E += VLJ_jit(v[i], v[j])    # using jit actually shaved off 20% form runtime
-    
-    return E   
 
 
 # Leonnard-Jones Potential in natural system of units
@@ -53,10 +42,10 @@ def VLJ_r(r):
 
 
 
-# TODO remove unnecessry ones whern done
-grad_Epot = jit(grad(Epot))  # https://jax.readthedocs.io/en/latest/jax.html#jax.grad
-Epot_jit = jit(Epot)
-VLJ_jit = jit(VLJ)
+# https://jax.readthedocs.io/en/latest/jax.html#jax.grad
+Epot_jit = jax.jit(Epot)
+grad_Epot = grad(Epot)
+grad_Epot_jit = jit(grad(Epot))  
 
 class Domain:
     # Domain settings
@@ -69,13 +58,20 @@ class Domain:
     # Stats
     time_minimizeEnergy = float
 
-    def __init__(self):
+    # Physics
+    Epot = object
+    grad_Epot = object
+
+    def __init__(self, fEpot=Epot_jit, fGradEpot=grad_Epot_jit):
         self.particle_count = -1
         self.length = -1
         self.std_dev = -1
 
         # Benchmark stats
         self.time_minimizeEnergy = -1
+
+        self.Epot = fEpot
+        self.grad_Epot = fEpot
 
     def fill(self, particle_count, length, std_dev):
         # TODO: Move length arguemnt to constructor
@@ -109,7 +105,14 @@ class Domain:
             #for j in range(3):
                 #self.pos[i][j] = (spacing / 2 + i*spacing/10) / 5 # + 1/2 to avoid placing particles at (0|0)
 
-        self.pos = self.vel = np.random.rand(self.particle_count, 3) * self.length
+        self.pos =  np.random.rand(self.particle_count, 3) * self.length
+        #posx = np.linspace(0, self.length, self.particle_count)
+        #posy = np.linspace(0, self.length, self.particle_count)
+        #posz = np.linspace(0, self.length, self.particle_count)
+
+        #pos = np.array([posx, posy, posz])
+        #self.pos = pos.T
+
 
     def initialize_vel(self):
         # TODO, Reno: replace with custom distriburtion from Task 2.3 and 2.4
@@ -118,11 +121,11 @@ class Domain:
     def minimizeEnergy(self):
         start = time.time()
         result = minimize(
-            Epot, 
+            self.Epot, 
             self.pos.ravel(),
-            jac=grad_Epot,
+            jac=self.grad_Epot,
             method='CG',
-            options={"disp" : True, "maxiter" : 5}           
+            options={"disp" : True, "maxiter" : 10}           
         )
 
         new_pos = to3D(result.x)
@@ -184,9 +187,13 @@ class Domain:
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
+    N = 20
+    rho = 0.8
+
+    L = ((float(N)/rho))**(1.0/3.0)
     np.random.seed(1)
-    domain = Domain()
-    domain.fill(5, 9, 1)   
+    domain = Domain(Epot_jit, grad_Epot_jit)
+    domain.fill(20, 20, 1)   
 
     old_energy = Epot(domain.pos)
     old_pos = domain.pos
