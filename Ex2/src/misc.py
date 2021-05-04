ee779392880764e6d2fe47115ad8fdff4e56fd79
hitimr <hitimr@gmail.com>
2021-05-04 19:04:02 +0200

    added root dir variable

--- Ex2/src/misc.py
+++ Ex2/src/misc.py
@@ -1,6 +1,17 @@
 import numpy as np
 import jax.numpy as jnp
+import os
+import platform
+import inspect
 
+# System specific directory separator
+if platform.system() in ["Darwin", "Linux"]:
+    SEP = "/"
+else: # platform.system() == "Windows":
+    SEP = "\\"
+
+DIR_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))) + SEP
+DIR_OUT = DIR_ROOT + "out" + SEP
 
 def to3D(v):
     """Convert a 1D data to matrix (array of vector3)
