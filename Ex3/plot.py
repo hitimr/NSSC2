9751d40eca2196f7cf9068539554facab66d9a96
markushk <34336941+markushk@users.noreply.github.com>
2021-05-26 00:30:44 +0200

    Update plot.py

--- Ex3/plot.py
+++ Ex3/plot.py
@@ -23,6 +23,7 @@ def plot_num_iter():
         for b in num_iter:
             C_tmp=np.loadtxt(DIR_OUT + "Nx_100_dt_20_numIter_"+str(b)+"_task_"+str(a)+".txt")
             axs.plot(np.linspace(0,1, 100),C_tmp[-1],label="num_iter = "+str(b))
+            plt.title("C for different number of iterations task1."+str(a))
         plt.legend(loc="upper right")
         plt.savefig(DIR_OUT +"task1_"+str(a)+"numIter_task.png")
         #plt.show()
@@ -32,6 +33,7 @@ def plot_num_vs_analytical():
     t=10000
     result_analitic = tf1.analytical_sol(t, 100, 100)
     C=np.loadtxt(DIR_OUT + "Nx_100_dt_1_numIter_10000_task_1.txt")
+    plt.title("Numerical vs analytical solution")
     plt.plot(np.linspace(0,1, 100), result_analitic, label="analytic")
     plt.plot(np.linspace(0,1, 100), C[-1],marker='o',linestyle = 'None', label="numeric")
     plt.legend()
@@ -42,6 +44,7 @@ def plot_error():
     setup()
     t=10000
     C=np.loadtxt(DIR_OUT + "Nx_100_dt_1_numIter_10000_task_1.txt")
+    plt.title("Absolute error numerical and analytical solution")
     result_analitic = tf1.analytical_sol(t, 100, 100)
     plt.plot(np.linspace(0,1, 100),np.abs(result_analitic-C[-1]))
     plt.savefig(DIR_OUT + "task_1_1_error.png")
@@ -51,6 +54,7 @@ def plot_t_infinity():
     setup()
     C1=np.loadtxt(DIR_OUT + "Nx_100_dt_20_numIter_20000_task_1.txt")
     C2=np.loadtxt(DIR_OUT + "Nx_100_dt_20_numIter_20000_task_2.txt")
+    plt.title("Behavior for large t")
     plt.plot(np.linspace(0,1, 100), C1[-1], label="analytic")
     plt.plot(np.linspace(0,1, 100), C2[-1], label="numeric")
     plt.legend()
@@ -60,6 +64,7 @@ def plot_t_infinity():
 def plot_unstable():
     setup()
     C=np.loadtxt(DIR_OUT + "Nx_100_dt_51_numIter_5000_task_1.txt")
+    plt.title("Unstable solution for explicit solution")
     plt.plot(np.linspace(0,1, 100), C[-1], label="numeric unstable")
     plt.legend()
     plt.savefig(DIR_OUT + "task_1_1_unstable.png")
